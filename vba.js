d50b4130ab0e8e5318f2dd6283ceffe1d0ae3ece
Andrew <liboan1997@gmail.com>
2014-06-27 09:53:57 -0400

    fixed pie center div- actually tested

--- vba.js
+++ vba.js
@@ -308,10 +308,7 @@ function svgPie(data) {
 	var max = data[8][0] + data[8][1];
 	var distScale = d3.scale.linear().domain([0, Math.floor(data[8][1] + data[8][0])]).range([0,2*Math.PI]); 
 
-	/*var middle = d3.select("#svgPieWrapper")
-					.append("div")
-					.attr("class","piemiddle")
-					.style("opacity", 1);*/
+	var middle = d3.select(".piemiddle");
 
 	var arc = d3.svg.arc()
 				.innerRadius(50)
